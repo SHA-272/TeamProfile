dd3ac4057c498be986b1797e475e0584922e5d63
magecode <magecode7@yandex.ru>
2023-09-27 16:41:22 +0300

    fix

--- app.py
+++ app.py
@@ -4,26 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    team_data = {
-        "name": "SHA-272",
-        "members": [
-            {"name": "magecode", "role": "Reverse, PPC, PWN", "achievements": "Writing code is like magic, it creates wonders"},
-            {"name": "nik_kir1", "role": "WEB, Promt ingeniring", "achievements": "laziness won and was filled, but not this is not for sure"},
-            {"name": "Shinbae", "role": "Cryptography Guru", "achievements": "RSA-dodik"},
-            {"name": "_muffin", "role": "WEB", "achievements": "A champion and a winner in everything"},
-            {"name": "damned", "role": "Forensic", "achievements": "Your deleted data will become mine"}
-        ],
-        "contact": "hackerselite@email.com"}
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT name, score FROM events")
+    cursor.execute("SELECT name, score, place FROM events")
     fetched_data = cursor.fetchall()
-    events = [{"name": name, "score": score} for name, score in fetched_data]
+    events = [{"name": name, "score": score, "place": place} for name, score, place in fetched_data]
+
+    cursor.execute("SELECT name, role, achievements FROM members")
+    fetched_data = cursor.fetchall()
+    members = [{"name": name, "role": role, "achievements": achievements} for name, role, achievements in fetched_data]
 
     conn.close()
 
-    return render_template('index.html', team=team_data, events=events)
+    return render_template('index.html', members=members, events=events)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)

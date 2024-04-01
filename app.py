60933018b7b826f3fcdd42feb095679ec645c03f
magecode <magecode7@yandex.ru>
2024-04-01 13:14:36 +0300

    f

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
-import sqlite3
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
+
+def load_json(filename):
+    with open(filename, "r") as json_file:
+        return json.load(json_file)
+
+
+@app.route("/")
 def home():
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
+    events = load_json("data/events.json")
+    members = load_json("data/members.json")
 
-    cursor.execute("SELECT name, role, quote, link FROM members")
-    fetched_data = cursor.fetchall()
-    members = [{"name": name, "role": role, "quote": quote, "link": link} for name, role, quote, link in fetched_data]
-    
-    cursor.execute("SELECT name, score, place, date FROM events ORDER BY date DESC")
-    fetched_data = cursor.fetchall()
-    events = [{"name": name, "score": score, "place": place, "date": date} for name, score, place, date in fetched_data]
+    events = sorted(events, key=lambda x: x["date"], reverse=True)
 
-    conn.close()
+    return render_template("index.html", members=members, events=events)
 
-    return render_template('index.html', members=members, events=events)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
